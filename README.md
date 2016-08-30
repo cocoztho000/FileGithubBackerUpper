cbdd5dae5cefff73aecbb5edd2a074fc22fe4873
Tom Cocozzello <thomas.cocozzello@gmail.com>
2016-08-29 22:09:42 -0500

    2016-08-29 22:09:42.264000

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ pip install python-crontab
 ```
 git config credential.helper store
 ```
+
+6) create windows schtasks
+```
+schtasks /Create /ST 16:00 /ET 18:00 /TN FileBackerUpper /SC minute /MO 10 /TR "python C:\Users\Tom\Desktop\file_backup\file_backup\file_backup.py"
+```
+
+7) how to see schtasks
+```
+schtasks /Query /TN filebackerupper
+```
--- file_backup/file_backup.py
+++ file_backup/file_backup.py
@@ -16,10 +16,10 @@ class fileBackerUpper(object):
         return proc.communicate()[0]
 
     def git_commit_changes(self):
+        self._execute_bash_cmd('cd C:\Users\Tom\Desktop\file_backup')
         self._execute_bash_cmd('git add .')
         self._execute_bash_cmd('git commit -a -m "' + str(datetime.datetime.now()) + '"')
         self._execute_bash_cmd('git push origin master')
-
 
 if __name__ == '__main__':
     fileBackerUpper().git_commit_changes()
