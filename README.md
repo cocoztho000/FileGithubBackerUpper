cd6c728cd20863d26fc940ac85c5b3f267fe6d99
Tom Cocozzello <thomas.cocozzello@gmail.com>
2016-08-29 21:19:48 -0500

    2016-08-29 21:19:48.051000

---
+++ README.md
@@ -0,0 +1,8 @@
+1) Make a git repo
+2) make sure python is setup on computer
+3) set `pip`, `python` and `git` in environment path on windows system
+
+3) install deps
+```
+pip install python-crontab
+```
---
+++ file_backup/file_backup.py
@@ -0,0 +1,25 @@
+
+import datetime
+import subprocess 
+
+from crontab import CronTab
+
+class fileBackerUpper(object):
+    def __init__(self):
+        mem_cron = CronTab(tab="""
+          * * * * * command
+        """)
+        mem_cron.write()
+
+    def _execute_bash_cmd(self, cmd):
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        return proc.communicate()[0]
+
+    def git_commit_changes(self):
+        self._execute_bash_cmd('git add .')
+        self._execute_bash_cmd('git commit -a -m "' + str(datetime.datetime.now()) + '"')
+        self._execute_bash_cmd('git push origin master')
+
+
+if __name__ == '__main__':
+    fileBackerUpper().git_commit_changes()
